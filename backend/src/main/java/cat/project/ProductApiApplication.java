c0f2b91612d3b3036119d273e232853c4fc69b97
nazarbodak221 <Nazak.bodak1993@gmail.com>
2025-09-03 19:09:41 +0300

    fix: application.properties update

--- backend/src/main/java/cat/project/ProductApiApplication.java
+++ backend/src/main/java/cat/project/ProductApiApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ProductApiApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(AppexoftTestTaskApplication.class, args);
+        SpringApplication.run(ProductApiApplication.class, args);
     }
 
 }
--- backend/src/test/java/cat/project/ProductApiApplicationTests.java
+++ backend/src/test/java/cat/project/ProductApiApplicationTests.java
@@ -1,10 +1,10 @@
-package cat.project.appexofttesttask;
+package cat.project;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class AppexoftTestTaskApplicationTests {
+class ProductApiApplicationTests {
 
     @Test
     void contextLoads() {
