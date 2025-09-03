6ac77e78f00626949fd4f1b6be0ebf4f43d10bd4
nazarbodak221 <Nazak.bodak1993@gmail.com>
2025-09-03 17:48:10 +0300

    feat: adding auth interceptor for tokens

---
+++ frontend/src/app/auth/interceptors/auth-interceptor.ts
@@ -0,0 +1,20 @@
+import { HttpInterceptorFn } from '@angular/common/http';
+
+export const authInterceptor: HttpInterceptorFn = (req, next) => {
+  const token = localStorage.getItem('token_data');
+
+  const isPublicRoute =
+    req.url.includes('/auth/login') || req.url.includes('/auth/register');
+
+  if (isPublicRoute || !token) {
+    return next(req);
+  }
+
+  const cloned = req.clone({
+    setHeaders: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  return next(cloned);
+};
