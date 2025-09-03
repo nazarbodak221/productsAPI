34253abc5390c61eabfdbb53e8826df7e967dbf5
nazarbodak221 <Nazak.bodak1993@gmail.com>
2025-09-03 17:33:36 +0300

    initial backend commit

---
+++ backend/src/main/java/cat/project/AppexoftTestTaskApplication.java
@@ -0,0 +1,13 @@
+package cat.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppexoftTestTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppexoftTestTaskApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/cat/project/appexofttesttask/AppexoftTestTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package cat.project.appexofttesttask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppexoftTestTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
